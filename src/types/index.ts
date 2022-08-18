cfeb60cef6e9466992809c6746a83e59916b0235
Toti Muñoz <totigm@gmail.com>
2022-08-18 17:09:08 -0300

    👽️ Update types for @totigm/bot-builder options

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,7 +1,7 @@
 import { ClientOptions } from "whatsapp-web.js";
 import { Options as BotBuilderOptions } from "@totigm/bot-builder";
 
-type BotOptions = Omit<BotBuilderOptions, "contentProp" | "textFormatting"> & {
+type BotOptions = Omit<BotBuilderOptions, "contentProp" | "textFormatting" | "authData"> & {
     messageEvent?: "message" | "message_create";
 };
 
