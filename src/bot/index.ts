e3c7e548e0520eb6363b1cd80254dcaefc47209d
Toti Muñoz <totigm@gmail.com>
2022-08-29 20:27:15 -0300

    ✨ Add docker support

--- src/bot/index.ts
+++ src/bot/index.ts
@@ -2,14 +2,27 @@ import Bot from "@totigm/bot-builder";
 import qrcode from "qrcode-terminal";
 import { Client, LocalAuth, Message } from "whatsapp-web.js";
 import { WhatsappBotOptions } from "../types";
+import fs from "fs";
 
 export default class WhatsappBot extends Bot<Client, Message> {
     // Add auth options with clientId (see https://wwebjs.dev/guide/authentication.html#multiple-sessions)
     constructor(options: WhatsappBotOptions = {}) {
-        const client = new Client({
+        const dockerOptions = {
+            puppeteer: {
+                executablePath: process.env.CHROME_BIN ?? "/usr/bin/chromium-browser",
+                args: ["--no-sandbox"],
+            },
+        };
+
+        const isDockerContainer = fs.existsSync("/.dockerenv");
+
+        const clientOptions = {
             authStrategy: new LocalAuth(),
+            ...(isDockerContainer ? dockerOptions : {}),
             ...options.client,
-        });
+        };
+
+        const client = new Client(clientOptions);
         super(client, {
             contentProp: "body",
             messageEvent: "message",
